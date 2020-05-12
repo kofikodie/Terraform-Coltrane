21ca3e6a7caaf1b116a24a24a41c77a08b22bf69
kaddo <kofi.addo@facile.it>
2020-05-12 20:37:09 +0200

    feat: convert to HC2

--- software-prov/instance.tf
+++ software-prov/instance.tf
@@ -15,13 +15,10 @@ resource "aws_instance" "example" {
   provisioner "remote-exec" {
     inline = [
       "chmod +x /tmp/script.sh",
-      "sudo sed -i -e 's/\r$//' /tmp/script.sh", # Remove the spurious CR characters.
       "sudo /tmp/script.sh",
     ]
   }
   connection {
-    host        = coalesce(self.public_ip, self.private_ip)
-    type        = "ssh"
     user        = var.INSTANCE_USERNAME
     private_key = file(var.PATH_TO_PRIVATE_KEY)
   }
